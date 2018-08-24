58106d5d91f4930495515ed7216a2773423730ef
chuck.mersereau <chuck.mersereau@MacBook-C02X70Q4JHD5.local>
2018-08-24 14:45:39 -0400

    tired

--- gitpractice.py
+++ gitpractice.py
@@ -5,11 +5,7 @@ x = 5
 i = 1
 y = 2
 z = 0
-<<<<<<< HEAD
 a = 20
-=======
-a =20
->>>>>>> 82b21a6f620d6151ae6a681f6dbc33930926e173
 
 # a comment for testing
 # another comment for testing
@@ -27,7 +23,4 @@ print(x)
 a = 15
 
 x = x * z
-print(x)
-
-x = a
 print(x)
